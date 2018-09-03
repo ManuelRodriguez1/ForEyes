95f782bb8e94e144d0f7d8d8564090dd4ce80dba
Unknown <katheryn@nativo.design>
2018-09-03 09:29:03 -0500

    componentes estaciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WinterComponent } from './winter/winter.component';
+import { SpringComponent } from './spring/spring.component';
+import { SummerComponent } from './summer/summer.component';
+import { FallComponent } from './fall/fall.component';
+import { HolidayComponent } from './holiday/holiday.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WinterComponent,
+    SpringComponent,
+    SummerComponent,
+    FallComponent,
+    HolidayComponent
   ],
   imports: [
     BrowserModule
